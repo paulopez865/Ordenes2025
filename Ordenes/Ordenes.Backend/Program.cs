0ca3b7b0fe76be70d6410b85e9e2d596c705554b
paulopez865 <informaticasorteosuabc@gmail.com>
2025-12-03 15:09:33 -0800

    create database whith the first entyty cunties

--- Ordenes/Ordenes.Backend/Program.cs
+++ Ordenes/Ordenes.Backend/Program.cs
@@ -1,23 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Ordenes.Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
