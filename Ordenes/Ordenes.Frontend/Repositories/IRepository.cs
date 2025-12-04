0d6ea5cf042116810c399d2774bbdfc801efaa4a
paulopez865 <informaticasorteosuabc@gmail.com>
2025-12-04 11:40:41 -0800

    Begin Repository pattern

---
+++ Ordenes/Ordenes.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordenes.Frontend.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(String url);
+        Task<HttpResponseWrapper<object>> PostAsync<T>(String url, T model);
+        Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
+    }
+}
